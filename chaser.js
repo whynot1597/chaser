a3e6fab2cdba3f985df7e2496ff528f27264648d
whynot1597 <tahoesoccerdefender@yahoo.com>
2017-12-05 14:28:26 -0800

    Update chaser.js

--- chaser.js
+++ chaser.js
@@ -170,18 +170,18 @@ function loadHighscores() {
 
 function getCookie(cookieName) {
   let name = cookieName + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
 
 function restartGame() {
