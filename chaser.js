32ec2ce0e57e3bbabcf33fb688763cf318c2e19c
Andrey-Varakin <33736955+Andrey-Varakin@users.noreply.github.com>
2017-12-08 21:21:31 -0800

    Updated JS

--- chaser.js
+++ chaser.js
@@ -1,23 +1,41 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const progressBar = document.getElementById("progressBar");
-const healthPercentage = document.getElementById("healthPercentage");
-healthPercentage.innerHTML = progressBar.value;
-const numberOfEnemies = document.getElementById("numberOfEnemies");
-const backgroundSong = document.getElementById("backgroundSong");
-const damageSoundEffect = document.getElementById("damageSoundEffect");
-let enemies = [];
-let addHealthInterval = undefined;
-let addEnemiesInterval = undefined;
-let isInvincible = false;
-let playerIsAlive = true;
-let highscore = 0;
-numberOfEnemies.innerHTML = enemies.length;
+const progressBar = document.querySelector("meter");
+const highScoreTable = document.getElementById("highScores");
+let enemyCount = 0;
+let powerupCount = 0;
+let score = 0;
+startGame();
 
-function addHealth() {
-  if (progressBar.value <= 95) {
-    progressBar.value += 5;
-    healthPercentage.innerHTML = progressBar.value;
+function startGame() {
+  initializeHighScoresToZero();
+  initializeTimers();
+};
+
+function initializeTimers() {
+  enemyTimer = setInterval(addEnemy, 1000);
+  powerupTimer = setInterval(addPowerup, 20000);
+  shrinkCanvasTimer = setInterval(shrinkCanvas, 100);
+  scoreTimer = setInterval(incrementScore, 100);
+};
+
+function initializeHighScoresToZero() {
+  for (let placeNumber = 1; placeNumber <= 4; placeNumber++) {
+    highScoreTable.rows[1].cells[placeNumber - 1].value = 0;
+    document.getElementById(`place${placeNumber}`).innerHTML = 0;
+  }
+}
+
+function restartGame() {
+  if (progressBar.value === 0) {
+    score = 0;
+    resetCanvas();
+    clearBackground();
+    deleteEnemies();
+    deletePowerups();
+    initializeTimers();
+    progressBar.value = progressBar.max;
+    requestAnimationFrame(drawScene);
   }
 }
 
@@ -25,8 +43,12 @@ function distanceBetween(sprite1, sprite2) {
   return Math.hypot(sprite1.x - sprite2.x, sprite1.y - sprite2.y);
 }
 
-function haveCollided(sprite1, sprite2) {
+function haveCollidedWithEnemies(sprite1, sprite2) {
   return distanceBetween(sprite1, sprite2) < sprite1.radius + sprite2.radius;
+}
+
+function haveCollidedWithPowerups(sprite1, sprite2) {
+  return distanceBetween(sprite1, sprite2) < sprite1.width + sprite2.radius;
 }
 
 class Sprite {
@@ -35,57 +57,87 @@ class Sprite {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fill();
-    ctx.strokeStyle = "white";
     ctx.stroke();
   }
 }
 
-function getPosition() {
-  let firstDimension = Math.floor(Math.random() * 600);
-  let secondDimension = 0;
-  if (getRandomBoolean()) {
-    secondDimension = 600;
-  }
-  this.x = firstDimension;
-  this.y = secondDimension;
-  if (getRandomBoolean()) {
-    this.x = secondDimension;
-    this.y = firstDimension;
-  }
-}
-
-function getRandomBoolean() {
-  return Math.random() < 0.5;
-}
-
 class Player extends Sprite {
+  constructor(x, y, radius, color, speed) {
+    super();
+    Object.assign(this, {x, y, radius, color, speed });
+  }
+}
+
+let player = new Player(250, 150, 15, "lemonchiffon", 0.5);
+
+class Enemy extends Sprite {
   constructor(x, y, radius, color, speed) {
     super();
     Object.assign(this, { x, y, radius, color, speed });
   }
 }
 
-let player = new Player(250, 150, 25, "red", 0.5);
+let enemies = [];
 
-class Enemy extends Sprite {
-  constructor() {
-    super();
-    getPosition();
-    let radius = Math.floor(Math.random() * 10 + 10);
-    let speed = Math.random() / 10;
-    let color = `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(
-      Math.random() * 255
-    )}, ${Math.floor(Math.random() * 255)}, ${Math.random()})`;
-    Object.assign(this, { x, y, radius, color, speed });
+addEnemy();
+
+function addEnemy() {
+  enemies.push(
+    new Enemy(
+      Math.floor(Math.random() * 800),
+      Math.floor(Math.random() * 800),
+      10 + Math.floor(Math.random() * 20),
+      "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+      Math.random() * 0.1
+    )
+  );
+  enemyCount++;
+}
+
+function deleteEnemies() {
+  enemies.splice(1, enemyCount);
+  enemyCount = 0;
+}
+
+class Powerup {
+  constructor(x, y, width, height, color) {
+    Object.assign(this, { x, y, width, height, color });
   }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = "white";
+    ctx.fillRect(this.x, this.y, this.width / 2, this.height);
+    ctx.stroke();
+  }
+}
+
+let powerups = [];
+
+function addPowerup() {
+  powerups.push(
+    new Powerup(
+      Math.floor(Math.random() * canvas.width),
+      Math.floor(Math.random() * canvas.height),
+      20,
+      20,
+      "red"
+    )
+  );
+  powerupCount++;
+}
+
+function deletePowerups() {
+  powerups.splice(0, powerupCount);
+  powerupCount = 0;
 }
 
 let mouse = { x: 0, y: 0 };
 document.body.addEventListener("mousemove", updateMouse);
 function updateMouse(event) {
-  const canvasRectangle = canvas.getBoundingClientRect();
-  mouse.x = event.clientX - canvasRectangle.left;
-  mouse.y = event.clientY - canvasRectangle.top;
+  const { left, top } = canvas.getBoundingClientRect();
+  mouse.x = event.clientX - left;
+  mouse.y = event.clientY - top;
 }
 
 function moveToward(leader, follower, speed) {
@@ -93,133 +145,160 @@ function moveToward(leader, follower, speed) {
   follower.y += (leader.y - follower.y) * speed;
 }
 
-function clearBackground() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+function noEnemyCollision(c1, c2) {
+  let [dx, dy] = [c2.x - c1.x, c2.y - c1.y];
+  const L = Math.hypot(dx, dy);
+  let distToMove = c1.radius + c2.radius - L;
+  if (distToMove > 0) {
+    dx /= L;
+    dy /= L;
+    c1.x -= dx * distToMove / 2;
+    c1.y -= dy * distToMove / 2;
+    c2.x += dx * distToMove / 2;
+    c2.y += dy * distToMove / 2;
+  }
 }
 
-function addEnemy() {
-  enemies.push(new Enemy());
-  numberOfEnemies.innerHTML = enemies.length;
+function incrementScore() {
+  score++;
+  document.getElementById("score").innerHTML = score;
+}
+
+/*function updateHighScore() {
+  if (score > highScoreTable.rows[1].cells[0].value) {
+    for (let placeNumber = 1; placeNumber <= 3; placeNumber++) {
+      highScoreTable.rows[1].cells[placeNumber].value =
+        highScoreTable.rows[1].cells[placeNumber - 1].value;
+      highScoreTable.rows[1].cells[placeNumber].innerHTML =
+        highScoreTable.rows[1].cells[placeNumber].value;
+      console.log(highScoreTable.rows[1].cells[1].value);
+    }
+    highScoreTable.rows[1].cells[0].value = score;
+    highScoreTable.rows[1].cells[0].innerHTML = score;
+  }
+}
+*/
+/*
+function updateHighScore() {
+  if (score > highScoreTable.rows[1].cells[0].value) {
+    highScoreTable.rows[1].cells[0].value = score;
+    highScoreTable.rows[1].cells[0].innerHTML = score;
+  } else {
+      for (placeNumber = 1; placeNumber <= 2; placeNumber++) {
+        if (score >= highScoreTable.rows[1].cells[placeNumber].value) {
+          highScoreTable.rows[1].cells[placeNumber + 1].value =
+          highScoreTable.rows[1].cells[placeNumber].value;
+          highScoreTable.rows[1].cells[placeNumber].value = score;
+          highScoreTable.rows[1].cells[placeNumber].innerHTML =
+          highScoreTable.rows[1].cells[placeNumber].value;
+        }
+    }
+  }
+}
+*/
+
+function updateHighScore() {
+  for (let a = 1; a <= 4; a++) {
+    if (score > document.getElementById(`place${a}`).innerHTML) {
+      for (let b = 4; b > a; b--) {
+        document.getElementById(
+          `place${b}`
+        ).innerHTML = document.getElementById(`place${b - 1}`).innerHTML;
+        document.getElementById(`place${a}`).innerHTML = score;
+        return;
+      }
+    }
+  }
+}
+
+function takeDamage() {
+  enemies.forEach(enemy => {
+    if (haveCollidedWithEnemies(enemy, player)) {
+      progressBar.value -= 0.02;
+    }
+  });
+}
+
+function regenerateHealth() {
+  if (progressBar.value < progressBar.max && progressBar.value > 0.0005) {
+    progressBar.value += 0.0005;
+  }
+};
+
+function addHealthFromPowerup() {
+  powerups.forEach(powerup => {
+    if (haveCollidedWithPowerups(powerup, player)) {
+      progressBar.value += 25;
+      deletePowerups();
+    }
+  });
 }
 
 function updateScene() {
   moveToward(mouse, player, player.speed);
   enemies.forEach(enemy => moveToward(player, enemy, enemy.speed));
-  enemies.forEach(enemy => {
-    if (haveCollided(enemy, player) && isInvincible === false) {
-      progressBar.value -= 25;
-      healthPercentage.innerHTML = progressBar.value;
-      player.color = "white";
-      damageSoundEffect.play();
-      setTimeout(() => (player.color = "red"), 100);
-      isInvincible = true;
-      setTimeout(() => (isInvincible = false), 1000);
-    }
-  });
+  enemies.forEach((enemy, i) =>
+    noEnemyCollision(enemy, enemies[(i + 1) % enemies.length])
+  );
+  takeDamage();
+  regenerateHealth();
+  addHealthFromPowerup();
 }
 
-function endGame() {
-  playerIsAlive = false;
-  backgroundSong.pause();
-  backgroundSong.currentTime = 0;
-  window.clearInterval(addEnemyInterval);
-  window.clearInterval(addHealthInterval);
-  ctx.font = "50px spooky_light";
-  ctx.fillStyle = "red";
-  ctx.textAlign = "center";
-  ctx.strokeText("You are dead...", canvas.width / 2, canvas.height / 2);
-  ctx.fillText("You are dead...", canvas.width / 2, canvas.height / 2);
-  testForHighScore();
+function clearBackground() {
+  ctx.fillStyle = "lightgreen";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function testForHighScore() {
-  let score = enemies.length;
-  for (let a = 1; a <= 5; a++) {
-    if (score > document.getElementById(`score${a}`).innerHTML) {
-      let date = new Date();
-      for (let b = 5; b > a; b--) {
-        document.getElementById(
-          `score${b}`
-        ).innerHTML = document.getElementById(`score${b - 1}`).innerHTML;
-        document.getElementById(`date${b}`).innerHTML = document.getElementById(
-          `date${b - 1}`
-        ).innerHTML;
-      }
-      document.getElementById(`score${a}`).innerHTML = score;
-      document.getElementById(`date${a}`).innerHTML = `${date.getMonth() +
-        1}/${date.getDate()}/${date.getFullYear()}`;
-      storeHighscoresToCookies();
-      return;
-    }
+function shrinkCanvas() {
+  if (canvas.width > 300) {
+    canvas.width--;
   }
 }
 
-function storeHighscoresToCookies() {
-  let cookieScore = undefined;
-  let cookieDate = undefined;
-  for (let a = 1; a <= 5; a++) {
-    cookieScore = document.getElementById(`score${a}`).innerHTML;
-    cookieDate = document.getElementById(`date${a}`).innerHTML;
-    document.cookie = `score${a}=${cookieScore}`;
-    document.cookie = `date${a}=${cookieDate}`;
-  }
+function resetCanvas() {
+  canvas.width = 1800;
 }
 
-function loadHighscores() {
-  for (let a = 1; a <= 5; a++) {
-    document.getElementById(`score${a}`).innerHTML = getCookie(`score${a}`);
-    document.getElementById(`date${a}`).innerHTML = getCookie(`date${a}`);
-  }
+function generateDeathMessage() {
+  ctx.font = "20px impact";
+    ctx.fillStyle = "black";
+    ctx.fillText(
+      `Your shape managed to accumulate ${score} points`,
+      30,
+      canvas.height / 2
+    );
+    ctx.fillText(
+      `with ${enemyCount} enemies! (click to play again)`,
+      30,
+      canvas.height / 2 + 20
+    );
 }
 
-function getCookie(cookieName) {
-  let name = cookieName + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-function restartGame() {
-  if (!playerIsAlive) {
-    playerIsAlive = true;
-    enemies = [];
-    numberOfEnemies.innerHTML = enemies.length;
-    progressBar.value = 100;
-    healthPercentage.innerHTML = progressBar.value;
-    startIntervals();
-    backgroundSong.play();
-    requestAnimationFrame(drawScene);
-  }
-}
-
-function startIntervals() {
-  addEnemyInterval = setInterval(addEnemy, 2000);
-  addHealthInterval = setInterval(addHealth, 5000);
-}
-
-function drawScene() {
-  clearBackground();
-  player.draw();
-  enemies.forEach(enemy => enemy.draw());
-  updateScene();
-  if (progressBar.value <= 0) {
-    endGame();
+function onDeath() {
+  if (progressBar.value <= 0.0005) {
+    clearInterval(scoreTimer);
+    clearInterval(enemyTimer);
+    clearInterval(shrinkCanvasTimer);
+    clearInterval(powerupTimer);
+    updateHighScore();
+    generateDeathMessage();
   } else {
     requestAnimationFrame(drawScene);
   }
 }
 
-loadHighscores();
-backgroundSong.play();
-startIntervals();
+function drawScene() {
+  clearBackground();
+  powerups.forEach(powerup => powerup.draw());
+  player.draw();
+  enemies.forEach(enemy => enemy.draw());
+  updateScene();
+  onDeath();
+}
+
+canvas.addEventListener("click", restartGame);
 requestAnimationFrame(drawScene);
+
+//try using meter instead of the progress bar
+//strongly suggested to track highscores with indexedDB event listener?
