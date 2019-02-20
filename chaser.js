514388c883c4f801bde1a00d872dbfc0aea5300e
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-02-20 08:16:31 -0800

    Update chaser.js

--- chaser.js
+++ chaser.js
@@ -284,7 +284,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'https://whynot1597.github.io/chaser/?mode=select',
+  signInSuccessUrl: 'https://whynot1597.github.io/chaser/login.html',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
