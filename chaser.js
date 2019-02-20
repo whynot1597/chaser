6b6cfeef426c14f57ffb10d2e4e3256c311eb3fd
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-02-19 19:53:42 -0800

    Update chaser.js

--- chaser.js
+++ chaser.js
@@ -1,4 +1,4 @@
-let database = firebase.database();
+//let database = firebase.database();
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const progressBar = document.getElementById("progressBar");
