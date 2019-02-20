59e8ea76d7b817f0fa15d214564347dea35e7bcb
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-02-20 08:35:39 -0800

    Fix login link

--- chaser.js
+++ chaser.js
@@ -266,7 +266,7 @@ function writeUserData(userId, name, score, date) {
 }
 
 if (document.cookie.indexOf("CrewCentreSession=Valid") == -1) {
-  location.href = "chaser/login.html";
+  location.href = "/chaser/login.html";
 }
 loadHighscores();
 backgroundSong.play();
