c37e83083894e65b53dcc284926471214df56521
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-02-20 08:31:31 -0800

    Update chaser.js

--- chaser.js
+++ chaser.js
@@ -265,32 +265,10 @@ function writeUserData(userId, name, score, date) {
   });
 }
 
-
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult) {
-      loadHighscores();
-      backgroundSong.play();
-      startIntervals();
-      requestAnimationFrame(drawScene);
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: 'https://whynot1597.github.io/chaser/login.html',
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
-};
-ui.start('#firebaseui-auth-container', uiConfig);
+if (document.cookie.indexOf("CrewCentreSession=Valid") == -1) {
+  location.href = "/Login.html";
+}
+loadHighscores();
+backgroundSong.play();
+startIntervals();
+requestAnimationFrame(drawScene);
