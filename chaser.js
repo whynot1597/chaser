5cbac6e906c0afa6fe1229740ac29c37269be5d9
Jeremy Goldberg <tahoesoccerdefender@yahoo.com>
2019-02-19 20:13:28 -0800

    Update chaser.js

--- chaser.js
+++ chaser.js
@@ -189,7 +189,7 @@ function storeHighscoresToCookies() {
   }
 }*/
 function loadHighscores() {
-  document.getElementById(`score1`).innerHTML = firebase.firestore().collection('Highscores').First.Score;
+  document.getElementById(`score1`).innerHTML = firebase.firestore().collection('Highscores');
 }
 
 function getCookie(cookieName) {
